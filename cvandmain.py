1cbfbf0497a47cf83af211d72f1c05134b5fcf2e
Jihwan Jeong <77142346+Jihwan-01@users.noreply.github.com>
2024-11-15 15:37:25 +0900

    2nd plan (except HPE, judgement, ooo)

--- cvandmain.py
+++ cvandmain.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 from PIL import ImageTk, Image
 import numpy as np
-from tkinter import Tk, PhotoImage, Label, Entry, Button, Frame
+from tkinter import Tk, PhotoImage, Label, Entry, Button, Frame, Checkbutton, IntVar
 import time
 
 global flag_win
@@ -41,6 +41,7 @@ def Login_Window():
     btn1 = Button(login_win)
     btn1.config(width=10, height=1)
     btn1.config(text = "Login")
+    lab3 = Label(login_win, font = ("맑은고딕", "10"), background = "lightblue", fg="red")
     def login():
         user_ID = ent1.get()
         user_PW = ent2.get()
@@ -48,6 +49,9 @@ def Login_Window():
             global flag_win
             flag_win=4
             login_win.destroy()
+        else:
+            lab3.config(text="아이디와 비밀번호를 다시 확인해주세요")
+    lab3.pack()
     btn1.config(command=login)
     btn1.pack()
 
@@ -68,40 +72,68 @@ def Login_Window():
 
 def Join_Window():
     join_win = Tk()
-    join_win.geometry("540x260+420+200")
+    join_win.geometry("540x400+420+200")
     join_win.resizable(width=0, height=0)
     join_win.title("HPE_Join")
     join_win.configure(bg = 'lightblue')
     join_win.option_add("*Font", "맑은고딕 20")
 
-    lab0 = Label(join_win, font = ("맑은고딕", "20"), background = "lightblue")
-    lab0.config(text="Name Input")
-    lab0.pack()
-
-    ent1 = Entry(join_win, font=("맑은고딕", "20"))
-    ent1.pack()
-
     lab1 = Label(join_win, font = ("맑은고딕", "20"), background = "lightblue")
-    lab1.config(text="ID Input")
+    lab1.config(text="Name Input")
     lab1.pack()
-
     ent1 = Entry(join_win, font=("맑은고딕", "20"))
+    ent1.insert(0,"최성현")
+    def clear1(event):
+        if ent1.get() == "최성현":
+            ent1.delete(0,len(ent1.get()))
+    ent1.bind("<Button-1>",clear1)
     ent1.pack()
 
     lab2 = Label(join_win, font = ("맑은고딕", "20"), background = "lightblue")
-    lab2.config(text="PW Input")
+    lab2.config(text="ID Input")
     lab2.pack()
-
     ent2 = Entry(join_win, font=("맑은고딕", "20"))
+    ent2.insert(0,"doq1324@naver.com")
+    def clear2(event):
+        if ent2.get() == "doq1324@naver.com":
+            ent2.delete(0,len(ent2.get()))
+    ent2.bind("<Button-1>",clear2)
     ent2.pack()
 
+    lab3 = Label(join_win, font = ("맑은고딕", "20"), background = "lightblue")
+    lab3.config(text="PW Input")
+    lab3.pack()
+    ent3 = Entry(join_win, font=("맑은고딕", "20"))
+    ent3.config(show="*")
+    ent3.pack()
+
+    lab4 = Label(join_win, font = ("맑은고딕", "20"), background = "lightblue")
+    lab4.config(text="PW check")
+    lab4.pack()
+    ent4 = Entry(join_win, font=("맑은고딕", "20"))
+    ent4.config(show="*")
+    ent4.pack()
+
+    lab5 = Label(join_win, font = ("맑은고딕", "10"), background = "lightblue", fg="red")
     btn_a = Button(join_win)
     btn_a.config(width=10, height=1)
     btn_a.config(text = "Next")
     def click():
-        global flag_win
-        flag_win=3
-        join_win.destroy()
+        input_name = ent1.get()
+        input_ID = ent2.get()
+        input_Password = ent3.get()
+        Check_Password = ent4.get()
+        if (input_Password == Check_Password) and (len(input_name) != 0 and len(input_ID) != 0 and len(input_Password) != 0):
+            '''
+            데이터 베이스로 개인정보(이름, 아이디, 비밀번호) 넘기는 부분
+            '''
+            global flag_win
+            flag_win=3
+            join_win.destroy()
+        else:
+            lab5.config(text="입력 정보를 다시 확인해 주세요.")
+
+    lab5.pack()
     btn_a.config(command=click)
     btn_a.pack()
 
@@ -112,52 +144,66 @@ def Join_Window():
 
 def RegiPose_Window():
     regi_win = Tk()
-    regi_win.geometry("800x640+200+80")
-    regi_win.resizable(width=0, height=0)
-    regi_win.title("HPE_Register")
-    regi_win.configure(bg = 'lightblue')
+    regi_win.geometry("1000x600+100+100")
+    regi_win.title("HPE_Login")
+    regi_win.configure(bg='lightblue')
     regi_win.option_add("*Font", "맑은고딕 20")
 
-    # 동영상 라벨 만들기 파트
-    # 동영상 위의 단어
-    lab1 = Label(regi_win, font = ("맑은고딕", "15", "bold"), background = "lightblue")
-    lab1.config(text="\n<카메라를 10초간 응시하세요. 사용자 자세 등록을 실행 중 입니다.>\n")
+    lab1 = Label(regi_win, font=("맑은고딕", "10"), background="lightblue")
+    lab1.config(text="카메라를 10초간 응시하세요. 사용자 자세 등록을 실행 중 입니다.")
     lab1.pack()
-    # 동영상이 들어갈 프레임 선언
-    frm = Frame(regi_win, bg = "lightblue", width=120, height=120)
+
+    frm = Frame(regi_win, bg="lightblue", width=120, height=120)
     frm.pack()
-    # 동영상을 프레임에 위치 시킴
+
     lbl_video = Label(frm)
     lbl_video.pack()
-    # videocapture 객체를 통해 동영상 재생
+
     cap = cv2.VideoCapture(0)
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
     def video_play():
         success, image = cap.read()
         if not success:
             print("카메라 없음")
             return
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.flip(image, 1)
+
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+            results = pose.process(image)
+
+            if results.pose_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_pose.POSE_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
+                )
+        
         img = Image.fromarray(image)
         imgtk = ImageTk.PhotoImage(image=img)
-        # opencv 동영상을 tkinter에 넣음
         lbl_video.imgtk = imgtk
         lbl_video.configure(image=imgtk)
         lbl_video.after(10, video_play)
+
     video_play()
 
     btn_a = Button(regi_win)
     btn_a.config(width=10, height=1)
-    btn_a.config(text = "Register")
+    btn_a.config(text="Register")
+
     def click():
         global flag_win
-        flag_win=1
+        flag_win = 1
         regi_win.destroy()
+
     btn_a.config(command=click)
     btn_a.pack()
 
     regi_win.protocol('WM_DELETE_WINDOW', quit)
-
     regi_win.mainloop()
 
 
@@ -192,20 +238,40 @@ def Main_Window():
     lbl_video.pack()
     # videocapture 객체를 통해 동영상 재생
     cap = cv2.VideoCapture(0)
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+    # 체크박스 변수와 버튼을 regiPose_Window 내에서 정의
+    CheckVar1 = IntVar()
+    c1 = Checkbutton(main_win, text="Visualization", variable=CheckVar1, bg="lightblue", font=("Arial", "15", "bold"))
+    c1.place(x=545, y=680)
+
     def video_play():
         success, image = cap.read()
         if not success:
             print("카메라 없음")
             return
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # image = cv2.resize(image, dsize=(800, 533))
         image = cv2.flip(image, 1)
+
+        if CheckVar1.get() == 1:  # 체크박스가 선택된 경우 랜드마크 표시
+            with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+                results = pose.process(image)
+
+                if results.pose_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        results.pose_landmarks,
+                        mp_pose.POSE_CONNECTIONS,
+                        landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
+                    )
+        
         img = Image.fromarray(image)
         imgtk = ImageTk.PhotoImage(image=img)
-        # opencv 동영상을 tkinter에 넣음
         lbl_video.imgtk = imgtk
         lbl_video.configure(image=imgtk)
         lbl_video.after(10, video_play)
+
     video_play()
 
     img1 = PhotoImage(file="C:/Users/jihwa/Desktop/worksp/program/picture1.png", master=main_win)
